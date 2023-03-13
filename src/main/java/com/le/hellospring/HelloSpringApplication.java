c92a2941d7d2cbc3558d8276044de8d97d7a8d13
bprzybysz <blazej.przybyszewski@gmail.com>
2023-03-13 01:03:51 +0100

    hello-spring: still not running

---
+++ src/main/java/com/le/hellospring/HelloSpringApplication.java
@@ -0,0 +1,29 @@
+package com.le.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloSpringApplication {
+
+    @RequestMapping("/primes")
+    public String primes(@RequestParam(value = "n", defaultValue = "100") int n)
+    {
+        return Solution.primes(n);
+    }
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloSpringApplication.class, args);
+    }
+
+}
