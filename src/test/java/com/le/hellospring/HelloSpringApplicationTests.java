14b37a77545e9f6fff8d24a689e86fffc1e87be6
bprzybysz <blazej.przybyszewski@gmail.com>
2023-03-13 01:45:26 +0100

    hello-spring: running

--- src/test/java/com/le/hellospring/HelloSpringApplicationTests.java
+++ src/test/java/com/le/hellospring/HelloSpringApplicationTests.java
@@ -1,9 +1,14 @@
 package com.le.hellospring;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles(profiles = "localtest")
 class HelloSpringApplicationTests {
 
     @Test
